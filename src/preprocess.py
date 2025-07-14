f0e777b65f74a123d19e2115c19a86c28e595b3c
fred0103 <fxp395@student.bham.ac.uk>
2025-07-14 02:09:30 +0100

    Fixed issue with age+gender tags

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,14 +2,16 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm')
 
-doc = nlp("Test text for spacy. Has some [punctuation!] and other stuff")
+text = "I, 20M, am trying to process this text. Running into some issues, but no matter."
 
-# Some reddit specific changes we have to make:
-# It will be useful for us to keep tags like M23 and F31
-# Unfortunately, spacy will treats M and F differently when lemmatising
-# We need to substitute M and F with X and Y, since they get treated the same
-# TODO: Fix M and F
+doc = nlp(text)
 
+# It will be useful for us to keep tags like M23, F16, 12M, 43F
+# Spacy lemmatises 12M to 12 m so we need to merge manually
+with doc.retokenize() as retokenizer:
+    for i in range(len(doc)-1):
+        if doc[i].text.isalnum() and doc[i+1].text.lower() == "m":
+            retokenizer.merge(doc[i:i+2])
 
 # TODO: Decide whether or not to keep stopwords based on what gets better results
 processed_text = " ".join([token.lemma_.lower() for token in doc if token.text.isalnum() and not token.is_stop])
