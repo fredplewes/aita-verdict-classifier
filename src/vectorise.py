7d5c229fa1fa24f2e9323a703a911ddfe437d2bf
fred0103 <fxp395@student.bham.ac.uk>
2025-07-29 20:12:29 +0100

    Create basic vectorisation pipeline

---
+++ src/vectorise.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import hstack
+
+df = pd.read_csv("data/processed/aita_processed.csv")
+
+titles = df["title"].tolist()
+bodies = df["body"].tolist()
+
+vectoriser = TfidfVectorizer()
+X_title = vectoriser.fit_transform(titles)
+X_body = vectoriser.fit_transform(bodies)
+
+X = hstack([X_title, X_body])
