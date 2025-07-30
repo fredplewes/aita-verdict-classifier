da1b48d39619abd027d04658956c463529e97aea
fred0103 <fxp395@student.bham.ac.uk>
2025-07-30 16:19:17 +0100

    Save created vector as joblib file

--- src/vectorise.py
+++ src/vectorise.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import hstack
+import joblib
+import os
+
+os.makedirs(os.path.dirname("data/processed/feature_vector.joblib"), exist_ok=True)
 
 df = pd.read_csv("data/processed/aita_processed.csv")
 
@@ -12,3 +16,4 @@ X_title = vectoriser.fit_transform(titles)
 X_body = vectoriser.fit_transform(bodies)
 
 X = hstack([X_title, X_body])
+joblib.dump(X, "data/processed/feature_vector.joblib")
